73002caaf1a420dd3af4051f7b537f6e1976ac8f
KasunKVI <kkhettiarachchi10@gmail.com>
2023-10-22 23:05:13 +0530

    implement the navigation bar js

---
+++ assets/js/navigation_handler.js
@@ -0,0 +1,70 @@
+
+
+$('#customer_section').css('display', 'none');
+$('#item_section').css('display', 'none');
+$('#order_section').css('display', 'none');
+
+
+$('#home_link,#home_link1').on('click', () => {
+    $('#home').css('display', 'block');
+    $('#customer_section').css('display', 'none');
+    $('#item_section').css('display', 'none');
+    $('#order_section').css('display', 'none');
+    $('footer').css('display', 'block');
+})
+
+$('#item_link,#item_link1').on('click', () => {
+    $('#home').css('display', 'none');
+    $('#customer_section').css('display', 'none');
+    $('#item_section').css('display', 'block');
+    $('#order_section').css('display', 'none');
+    $('footer').css('display', 'none');
+})
+
+
+$('#customer_link, #customer_link1').on('click', () => {
+    $('#home').css('display', 'none');
+    $('#customer_section').css('display', 'block');
+    $('#item_section').css('display', 'none');
+    $('#order_section').css('display', 'none');
+    $('footer').css('display', 'none');
+})
+
+
+$('#order_link,#order_link1').on('click', () => {
+    $('#home').css('display', 'none');
+    $('#customer_section').css('display', 'none');
+    $('#item_section').css('display', 'none');
+    $('#order_section').css('display', 'block');
+    $('footer').css('display', 'none');
+})
+
+
+$("#nav-icon-open").on("click", () => {
+    $("#nav-icon-open").css("display", "none");
+    $("#nav-icon-close").css("display", "block");
+    $("#navbarMob").css("display", "block");
+
+    // Apply the transitioned styles
+    setTimeout(() => {
+        $("#nav-icon-close").css("opacity", "1");
+        $("#navbarMob").css("opacity", "1");
+        $("#navbarMob").css("transform", "translateX(0)");
+    }, 200); // Add a slight delay to allow the initial styles to take effect
+});
+
+$("#nav-icon-close").on("click", () => {
+    $("#nav-icon-open").css("display", "block");
+    $("#nav-icon-close").css("display", "none");
+
+
+    // Apply the transitioned styles
+    setTimeout(() => {
+        $("#nav-icon-close").css("opacity", "0");
+        $("#navbarMob").css("opacity", "0");
+        $("#navbarMob").css("transform", "translateX(20px)");
+    }, 200);
+});
+
+
+
