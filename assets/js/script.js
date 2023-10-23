a4a5275b4df50d8877612f148ec4bd64af7c5267
KasunKVI <kkhettiarachchi10@gmail.com>
2023-10-23 06:07:18 +0530

    implement the account details js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+
 $("#nav-icon-open").on("click", () => {
     $("#nav-icon-open").css("display", "none");
     $("#nav-icon-close").css("display", "block");
@@ -12,6 +13,7 @@ $("#nav-icon-open").on("click", () => {
 });
 
 $("#nav-icon-close").on("click", () => {
+
     $("#nav-icon-open").css("display", "block");
     $("#nav-icon-close").css("display", "none");
 
@@ -21,5 +23,33 @@ $("#nav-icon-close").on("click", () => {
         $("#nav-icon-close").css("opacity", "0");
         $("#navbarMob").css("opacity", "0");
         $("#navbarMob").css("transform", "translateX(20px)");
+    }, 200);
+});
+
+$("#nav-icon-setting").on("click", () => {
+
+    $("#account_details").css("display", "block");
+    $("#nav-icon-setting").css("display" , "none");
+    $("#nav-icon-setting-close").css("display" , "block");
+
+    // Apply the transitioned styles
+    setTimeout(() => {
+        $("#account_details").css("opacity", "1");
+        $("#nav-icon-setting-close").css("opacity" , "1");
+        $("#account_details").css("transform", "translateY(33vh");
+    }, 200);
+});
+
+$("#nav-icon-setting-close").on("click", () => {
+
+    // $("#account_details").css("display", "none");
+    $("#nav-icon-setting-close").css("display" , "none");
+    $("#nav-icon-setting").css("display" , "block");
+
+    // Apply the transitioned styles
+    setTimeout(() => {
+        $("#account_details").css("opacity", "0");
+        $("#nav-icon-setting-close").css("opacity" , "0");
+        $("#account_details").css("transform", "translateY(-40vh");
     }, 200);
 });
