2b314234d821d890441714ec87d681a19eaa6743
KasunKVI <kkhettiarachchi10@gmail.com>
2023-10-28 10:55:34 +0530

    implement the store model class

---
+++ model/StoreModel.js
@@ -0,0 +1,9 @@
+export class StoreModel {
+    constructor(item_id, qty, name, exp_date, size) {
+        this.item_id=item_id;
+        this.qty=qty;
+        this.name=name;
+        this.exp_date=exp_date;
+        this.size=size;
+    }
+}
